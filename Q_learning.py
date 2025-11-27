218afd2dc2ad66a6e2bde60dc236d3fae16325e7
Ziang Chen <jackchen050802@gmail.com>
2025-11-27 09:37:25 -0500

    hash function

--- Q_learning.py
+++ Q_learning.py
@@ -7,3 +7,15 @@ from collections import defaultdict
 import matplotlib.pyplot as plt
 from campus_gui import *
 
+
+def hash_obs(obs):
+    key = (
+        obs['position'][0],
+        obs['position'][1],
+        obs['weather'],
+        obs['layer'],
+        obs['can_toggle_layer'],
+        obs['nearest_crowd'][0],
+        bs['nearest_crowd'][1],
+    )
+    return key
